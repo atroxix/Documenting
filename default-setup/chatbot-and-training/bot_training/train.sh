17a8a6b6b4274178bfd65e7583aa5eb8fe63c3e7
Silver <silver.salonen@gmail.com>
2022-10-12 10:14:41 +0300

    Update train.sh
    I guess $USER variable is not needed by anything else outside the script?

--- default-setup/chatbot-and-training/bot_training/train.sh
+++ default-setup/chatbot-and-training/bot_training/train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#choo-choo!!
+DIR=/your/actual/directory
 
-# Replace $USER$ with user folder, where training files are located in
+cd $DIR || exit 1
 echo 'Started training' >> train.log
 rm -rf -v chatbot-train/data >> train.log
 rm -rf -v chatbot-train/models >> train.log
@@ -10,6 +10,7 @@ rm -f -v chatbot-train/domain.yml >> train.log
 cp -r -v  chatbot/data chatbot-train >> train.log
 cp -v chatbot/domain.yml chatbot-train >> train.log
 mkdir -v chatbot-train/models >> train.log
+
 docker compose up train-bot
 docker compose up test-bot
 docker compose down
